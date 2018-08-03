3d230d3ee6a9f6a75b44a0b9679c78593295e854
Ashley-Kristine Esseya <pocketfulofash@gmail.com>
2018-08-03 19:45:56 -0400

    Practiced for of loop

---
+++ forOfLoopChallenge.js
@@ -0,0 +1,8 @@
+let incomes =  [62000, 67000, 75000];
+
+for (let income of incomes){
+  income += 5000;
+}
+
+console.log(incomes);
+// for of loop not use to update values just to iterate through them 
